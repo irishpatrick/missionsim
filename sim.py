2b5d402f1e9948e6839f0c7521dbf57ee56841c3
Patrick Roche <rochep63@gmail.com>
2019-09-23 14:28:45 -0400

    loading motor profiles works

--- sim.py
+++ sim.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# see https://docs.scipy.org/doc/scipy/reference/generated/scipy.integrate.odeint.html
 
 import numpy as np
 from scipy.integrate import odeint, simps, trapz, cumtrapz, quad
@@ -9,21 +10,22 @@ class Rocket:
     def __init__(self):
         self.motor = Motor()
         self.mass = 10.0 # kilograms
-        self.cd = 0.7 # guess
+        self.cd = 1.2 # guess
         self.area = 0.018 # meters^2
 
     def load(self, fn):
         with open(fn, "r") as fp:
             line = fp.readline()
             while line:
+                line = line[:-1]
                 if line.startswith("mass="):
-                    self.mass = float(line.split("=")[1][:-1])
+                    self.mass = float(line.split("=")[1])
                 elif line.startswith("area="):
-                    self.area = float(line.split("=")[1][:-1])
+                    self.area = float(line.split("=")[1])
                 elif line.startswith("cd="):
-                    self.cd = float(line.split("=")[1][:-1])
+                    self.cd = float(line.split("=")[1])
                 elif line.startswith("motor="):
-                    self.motor.load("./motors/" + line.split("=")[1][:-1])
+                    self.motor.load("./motors/" + line.split("=")[1])
                 
                 line = fp.readline()
 
@@ -33,13 +35,41 @@ class Rocket:
 class Motor:
     def __init__(self):
         self.thrust = []
-        self.mass = []
+        self.time = []
+        self.mass_casing = 0
+        self.mass_prop = 0
 
     def load(self, fn):
+        print("loading motor ", fn)
         with open(fn, "r") as fp:
             line = fp.readline()
+            read_thrust_data = False
 
             while line:
+                line = line[:-1]
+                parts = line.split("=")
+                name = parts[0]
+                value = ""
+                if len(parts) > 1:
+                    value = parts[1]
+
+                if line.startswith("end thrust"):
+                    read_thrust_data = False
+
+                if read_thrust_data and line != "":
+                    force = float(line.split(" ")[0])
+                    time = float(line.split(" ")[1])
+
+                    self.time.append(time)
+                    self.thrust.append(force)
+
+                if line.startswith("begin thrust"):
+                    read_thrust_data = True
+
+                if name == "mass_casing":
+                    self.mass_casing = float(value)
+                elif name == "mass_prop":
+                    self.mas_prop = float(value)
 
                 line = fp.readline()
 
@@ -47,7 +77,14 @@ class Motor:
         return 2 
 
     def get_thrust(self, t):
-        return 200
+        low = self.time[0]
+        high = self.time[-1]
+        if t < low:
+            return 0
+        if t > high:
+            return 0
+
+        return interp1d(self.time, self.thrust, kind="cubic")(t)
 
 class World:
     def __init__(self):
@@ -59,9 +96,7 @@ class World:
         return self.atm * np.exp((-1.0 * 4.81069412e-26 * self.g * height) / (boltzmann * tmp))
 
 def acceleration(v, t, rocket, world):
-    thrust = 0
-    if (t < 2):
-        thrust = rocket.motor.get_thrust(t)
+    thrust = rocket.motor.get_thrust(t)
 
     at = thrust / rocket.get_mass(t)
     ag = world.g
@@ -77,12 +112,12 @@ if __name__ == "__main__":
     rocket.load("./rocket.txt")
 
     v0 = 0
-    t = np.linspace(0, 5, num=1000)
+    t = np.linspace(0, 34)
     v = odeint(acceleration, v0, t, args=(rocket, world,))
 
     x = cumtrapz(np.swapaxes(v,0,1)[0], t, initial=0)
     
-    plt.plot(t, v, "black", t, x, "red")
+    plt.plot(t, v, "blue", t, x, "black")
     plt.xlabel("time")
     plt.ylabel("v(t)")
 
