3e4db92f6c2f1c7c64a5f27bfbe59ebd2fe851d9
Patrick Roche <rochep63@gmail.com>
2019-09-24 18:09:55 -0400

    more tweaking, removed test motor

--- sim.py
+++ sim.py
@@ -2,7 +2,7 @@
 # see https://docs.scipy.org/doc/scipy/reference/generated/scipy.integrate.odeint.html
 
 import numpy as np
-from scipy.integrate import odeint, cumtrapz, trapz
+from scipy.integrate import *
 from scipy.optimize import *
 from scipy.interpolate import interp1d, barycentric_interpolate
 import matplotlib.pyplot as plt
@@ -83,7 +83,9 @@ class Motor:
 
     def calculate(self):
         self.impulse = cumtrapz(self.thrust, self.time, initial=0)
-        self.impulse_total = trapz(self.thrust, self.time)
+        #self.impulse_total = trapz(self.thrust, self.time)
+        self.impulse_total = 2760.8899
+        #print(self.impulse_total)
 
     def get_mass(self, t):
         if t < self.time[0]:
@@ -108,16 +110,30 @@ class Motor:
 class World:
     def __init__(self):
         self.g = 9.8 # m/s/s
-        self.atm = 101.3 # kPa
+        self.atm = 101300 # Pa
         self.p = 1.225 # density
+        self.mm = 0.0289644
+        #self.base = 472.1352
+        self.base = 0
+        self.R = 8.3144598
 
-    def get_pressure(self, height, tmp):
-        boltzmann = 1.38064852e-23
-        return self.atm * np.exp((-1.0 * 4.8156e-26 * self.g * height) / (boltzmann * tmp))
+    def get_kelvin(self, temp):
+        #return (temp - ((height / 304.8) * 2)) + 273.15
+        return temp + 273.15
+
+    def get_density(self, height, temp):
+        return self.p * np.exp((-self.g * self.mm * (height - self.base)) / (self.R * self.get_kelvin(temp)))
 
 def func(x, y):
-    return interp1d(x, y, kind="cubic", fill_value="extrapolate")
+    return interp1d(x, y, kind="quadratic", fill_value="extrapolate")
 
+def plot(f, x):
+    out = []
+    for i in range(len(x)):
+        out.append(f(x[i]))
+
+    return out
+    
 # the second order diffeq
 def model(v, t, rocket, world):
     x, dx = v
@@ -126,9 +142,8 @@ def model(v, t, rocket, world):
 
     at = thrust / rocket.get_mass(t)
     ag = world.g
-    ad = np.sign(dx) * (0.5 * rocket.cd * rocket.area * world.p * np.power(dx, 2)) / rocket.get_mass(t)
+    ad = np.sign(dx) * (0.5 * rocket.cd * rocket.area * world.get_density(x, 20) * np.power(dx, 2)) / rocket.get_mass(t)
     dvdt = [dx, at - ag - ad]
-
     return dvdt
 
 if __name__ == "__main__":
@@ -138,17 +153,36 @@ if __name__ == "__main__":
 
     v0 = [0, 0]
     t = np.linspace(0, 50, 1000)
-    solution = odeint(model, v0, t, args=(rocket, world,))
+    solution = odeint(model, v0, t, args=(rocket, world))
     x = solution[:, 0]
     v = solution[:, 1]
 
     end = leastsq(func(t, x), 30)[0][0]
 
     plt.plot(t, x, color="black", label="x(t)")
-    plt.plot(t, v, color="red", label="v(t)")
+    plt.plot(t, v, color="blue", label="v(t)")
+    #plt.plot(t, plot(, t), color="red", label="a(t)")
     plt.xlabel("t")
     plt.xlim([0, end])
     plt.ylim(-500)
     plt.grid()
 
+    """
+    fig, ax1 = plt.subplots()
+    
+    ax1.set_xlabel("time")
+    ax1.set_ylabel("x(t)", color="black")
+    ax1.plot(t, x, color="black")
+    ax1.tick_params(axis="y", labelcolor="black")
+    ax1.set_xlim([0, end])
+    ax1.set_ylim(0)
+
+    ax2 = ax1.twinx()
+
+    ax2.set_ylabel("v(t)", color="red")
+    ax2.plot(t, v, color="red")
+    ax2.tick_params(axis="y", labelcolor="red")
+
+    fig.tight_layout()
+    """
     plt.show()
